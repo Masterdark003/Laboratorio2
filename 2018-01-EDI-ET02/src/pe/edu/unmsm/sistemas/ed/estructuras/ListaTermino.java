3d8c90f4356061e51a98f8db84b025997f8c9446
Masterdark003 <Masterdark003@test.github.com>
2018-09-02 20:49:24 -0500

    version del crrano :v

---
+++ 2018-01-EDI-ET02/src/pe/edu/unmsm/sistemas/ed/estructuras/ListaTermino.java
@@ -0,0 +1,208 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.unmsm.sistemas.ed.estructuras;
+
+
+/**
+ *
+ * @author desandlope0
+ */
+public class ListaTermino {
+
+    private NodoTermino cabecera;
+
+    
+    public NodoTermino getCabecera() {
+        return cabecera;
+    }
+
+    public void setCabecera(NodoTermino cabecera) {
+        this.cabecera = cabecera;
+    }
+
+
+
+    
+    
+    
+
+    public void agregarTermino(Termino nuevoTermino) {
+        //Lógica 
+        if (cabecera==null)
+            cabecera=new NodoTermino(nuevoTermino);
+        else
+            cabecera=new NodoTermino(nuevoTermino, cabecera);
+    }
+
+    public double evaluar(double numero) {
+        //Lógica 
+        NodoTermino aux = cabecera;
+        double resultado=0;
+        int i = 0;        
+        while (aux != null) {
+            resultado += aux.termino.coeficiente*Math.pow(numero, aux.termino.exponente);
+            aux = aux.sgte;
+        }
+        return resultado;
+    }
+    
+    
+    @Override
+    public String toString() {
+        //Lógica 
+        NodoTermino aux = cabecera;
+        String pop="";
+        int i = 0;        
+        while (aux != null) {
+            if (i == 0) {
+                if (aux.termino.coeficiente == 0) {
+                    pop += "";
+
+                } else if (aux.termino.exponente == 0) {
+                    pop += aux.termino.coeficiente;
+                    i++;
+                } else {
+                    pop += aux.termino.coeficiente + "*x^" + aux.termino.exponente;
+                    i++;
+                }
+            } else {
+                if (aux.termino.coeficiente == 0) {
+                    pop += "";
+
+                } else if (aux.termino.exponente == 0) {
+                    pop += " + " + aux.termino.coeficiente;
+                    
+                } else {
+                    pop += " + " + aux.termino.coeficiente + "*x^" + aux.termino.exponente;
+                    
+                }
+            }
+
+            aux = aux.sgte;
+        }
+        return pop;
+    }
+    
+
+    
+    public String toStringIntegrado() {
+        //Lógica 
+        NodoTermino aux = cabecera;
+        String pop="";
+        int i = 0;        
+        while (aux != null) {
+            if (i == 0) {
+                if (aux.termino.coeficiente == 0) {
+                    pop += "";
+
+                } else if (aux.termino.exponente == 0) {
+                    pop += aux.termino.coeficiente + "*In(x)";
+                    i++;
+                } else {
+                    pop += aux.termino.coeficiente + "*x^" + aux.termino.exponente;
+                    i++;
+                }
+            } else {
+                if (aux.termino.coeficiente == 0) {
+                    pop += "";
+
+                } else if (aux.termino.exponente == 0) {
+                    pop += " + " + aux.termino.coeficiente + "In(x)";
+                    
+                } else {
+                    pop += " + " + aux.termino.coeficiente + "*x^" + aux.termino.exponente;
+                    
+                }
+            }
+
+            aux = aux.sgte;
+        }
+        return pop;
+    }
+
+    public ListaTermino derivar(ListaTermino polinomio, int numeroDerivaciones) {
+        //Lógica 
+        ListaTermino d = new ListaTermino();
+        for (int x = 1; x <= numeroDerivaciones; x++) {
+            NodoTermino aux = cabecera;
+
+            int i = 0;
+            int exp;
+            double coef;
+            while (aux != null) {
+
+                if(aux.termino.exponente==1){
+                    exp = aux.termino.exponente - 1;
+                    coef = aux.termino.coeficiente;
+                } else if(aux.termino.exponente==0){
+                           exp = 0;
+                           coef = 0;
+                       }else {
+                           exp = aux.termino.exponente - 1;
+                           coef = aux.termino.exponente * aux.termino.coeficiente;
+                }
+                                
+                Termino nuevoT = new Termino(exp, coef);
+                d.agregarTermino(nuevoT);
+                aux = aux.sgte;
+            }
+        }
+
+        return d;
+    }
+    
+    /*public double evaluarDerivada(double valorEvaluar, int numeroDerivaciones){
+    
+        return 0;
+    }*/
+
+    public ListaTermino integrar(int numeroIntegraciones) {
+        //Lógica 
+        ListaTermino d = new ListaTermino();
+        for (int x = 1; x <= numeroIntegraciones; x++) {
+            NodoTermino aux = cabecera;
+
+            int i = 0;
+            int exp;
+            double coef;
+            while (aux != null) {
+
+                if(aux.termino.exponente==-1){
+                    //para integrar x a la -1 we :'v
+                    exp = aux.termino.exponente + 1;
+                    coef = aux.termino.coeficiente;
+                } else {
+                           exp = aux.termino.exponente + 1;
+                           coef = aux.termino.coeficiente / exp;
+                }
+                                
+                Termino nuevoT = new Termino(exp, coef);
+                d.agregarTermino(nuevoT);
+                aux = aux.sgte;
+            }
+        }
+
+        return d;
+    
+    }
+
+
+    public static ListaTermino obtenerPolinomioLegendre(int gradoPolinomioLegendre) {
+        //Lógica 
+        return null;
+    }
+
+    public static ListaTermino obtenerBinomioNewtonDesarrollado(int potenciaBinomioNewton) {
+        //Lógica 
+        return null;
+    }
+
+    public static double integrarSigmoidalNumericamente(int limiteInferior, int limiteSuperior) {
+        //Lógica 
+        return 0;
+    }
+
+}
---
+++ 2018-01-EDI-ET02/src/pe/edu/unmsm/sistemas/ed/estructuras/NodoTermino.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.unmsm.sistemas.ed.estructuras;
+
+/**
+ *
+ * @author desandlope0
+ */
+public class NodoTermino {
+
+    Termino termino;
+    NodoTermino sgte;
+
+
+    public NodoTermino getSgte() {
+        return sgte;
+    }
+
+    public void setSgte(NodoTermino sgte) {
+        this.sgte = sgte;
+    }
+
+
+    
+    
+    
+
+    
+    
+    public NodoTermino(Termino termino){
+        this.termino = termino;
+        sgte = null;
+    }
+    public NodoTermino(Termino termino, NodoTermino sgte) {
+        this.termino = termino;
+        this.sgte = sgte;
+    }
+}
